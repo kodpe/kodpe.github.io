168414a0283cc34400d4714eb06c52c261294457
sloquet <debarbouyette@gmail.com>
2024-11-13 18:26:18 +0100

    save

--- umq/js/expertise.js
+++ umq/js/expertise.js
@@ -36,7 +36,8 @@ function getExpertiseForDomain(domainName, questionsCounts) {
 
                 if (data) {
                     const totalQuestions = questionsCounts[domainName];
-                    const expertiseScore = getScoreSigmoide(getExpScore(totalQuestions, data.nbAnswered, data.nbCorrectAnswers));
+                    // const expertiseScore = getScoreSigmoide(getExpScore(totalQuestions, data.nbAnswered, data.nbCorrectAnswers));
+                    const expertiseScore = getExpScore(totalQuestions, data.nbAnswered, data.nbCorrectAnswers);
                     if (Number.isNaN(expertiseScore))
                         expertiseScore = 0;
                     resolve({ domainName, expertiseScore }); // Résoudre avec le résultat
