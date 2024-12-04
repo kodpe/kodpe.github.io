5f64123eb87426aa2b22be019c13e1a10e15f853
sloquet <debarbouyette@gmail.com>
2024-12-04 08:34:50 +0100

    fwjioj

---
+++ ipoi/js/popup.js
@@ -0,0 +1,47 @@
+const popupOverlay = document.getElementById("popup-overlay");
+const showPopupButton = document.getElementById("show-popup");
+const confirmButton = popupOverlay.querySelector(".confirm");
+const cancelButton = popupOverlay.querySelector(".cancel");
+const popupContent = popupOverlay.querySelector(".popup");
+
+function showPopup() {
+    let popupWarning = document.getElementById("popup-warning");
+    if (!popupWarning) {
+        // console.error("L'élément 'popup-warning' n'a pas été trouvé dans le DOM.");
+        return;
+    }
+    popupWarning.innerHTML = ``;
+    if (currentSite.icon.includes("nsfw")) {
+        popupWarning.innerHTML += `
+        <span class="warntitle">NSFW WARNING</span><br>This site contains sexually explicit images or references and may not be suitable for all audiences.<br><br>
+        `;
+    }
+    if (currentSite.icon.includes("photowarning")) {
+        popupWarning.innerHTML += `
+        <span class="warntitle">PSE WARNING</span><br>This site contains flashing lights or images that may trigger seizures in people with photosensitive epilepsy.<br><br>
+        `;
+    }
+    popupOverlay.classList.add("visible");
+}
+
+function hidePopup() {
+    popupOverlay.classList.remove("visible");
+    currentSite = {};
+}
+
+showPopupButton.addEventListener("click", showPopup);
+cancelButton.addEventListener("click", hidePopup);
+
+confirmButton.addEventListener("click", () => {
+    // console.log("Action confirmée !");
+    window.open(currentSite.url, "_blank");
+    currentSite = {};
+    hidePopup();
+});
+
+popupOverlay.addEventListener("click", (e) => {
+    if (!popupContent.contains(e.target)) { // Si le clic est en dehors du contenu
+        // console.log("clic en dehors  !");
+        hidePopup();
+    }
+});
