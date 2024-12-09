a77f739b0aca9d1c46f429ec69c898b18bc95f1a
Kodp <sloquet@student.42.fr>
2024-12-09 02:29:53 +0100

    odiwfjergo

---
+++ bat/js/data.js
@@ -0,0 +1,48 @@
+const cats = [
+
+];
+
+const topics = [
+    {
+        name: "Bartosz Ciechanowski",
+        cat: "",
+        url: "https://ciechanow.ski",
+        img: "bar",
+        icon: ["coding"],
+        keywords: ["learn", "coding", "bartosz"],
+    },
+];
+
+
+/*
+staff : administrator/ moderator/ community manager/ animator/ technical support/ +wtf ideas
+
+multi dimensionnal search
+past/futur tmeporal search time jump
+
+TOP BAR
+Inscription | Connexion | Active members | Staff | Search | Create topic
+
+PAGE ROOT
+
+ROOT
+
+CATEGORIE ICON | CATEGORIE NAME | NB TOPICS | NB POSTS | LAST MSG DATE+NAME | WTF GRAPH |
+
+
+PAGE <CATEGORIE>
+
+ROOT / <CATEGORIE>
+
+FAKE PAGE ID BAR 1 2 3 4 5 6 7 8 9 10 10^2 10^3 10^4 10^5 10^6 10^7 10^8 10^9 10^10
+
+CATEGORIE ICON | CATEGORIE NAME | AUTHOR | NB POSTS | LAST MSG DATE+NAME | WTF STATUT |
+
+FAKE PAGE ID BAR 1 2 3 4 5 6 7 8 9 10 10^2 10^3 10^4 10^5 10^6 10^7 10^8 10^9 10^10
+
+
+pour mon faux forum artistique et parodique reverser au bot, la liste des topics seront affiches dans un tableau comme ceci
+
+CATEGORIE ICON | CATEGORIE NAME | NB TOPICS | NB POSTS | LAST MSG DATE+NAME | WTF GRAPH |
+
+*/
