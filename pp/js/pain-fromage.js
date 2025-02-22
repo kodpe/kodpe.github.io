ad64dd3ab7b8d5854d661ee6f73f9e73ba880130
sloquet <debarbouyette@gmail.com>
2025-02-22 17:09:57 +0100

    owifje

--- pp/js/pain-fromage.js
+++ pp/js/pain-fromage.js
@@ -549,7 +549,11 @@ img6 = document.getElementById("t6");
 img7 = document.getElementById("t7");
 img8 = document.getElementById("t8");
 img9 = document.getElementById("t9");
-var imgsTV = [img1, img2, img3, img4, img5, img6, img7, img8, img9];
+img10 = document.getElementById("t10");
+img11 = document.getElementById("t11");
+img12 = document.getElementById("t12");
+img13 = document.getElementById("t13");
+var imgsTV = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12, img13];
 
 function tv_roll() {
     // console.log('tv_roll()');
