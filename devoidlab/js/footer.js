86e4d5cefa6bb03a57d689d76fdbfb96637730b1
Kodp <you@example.com>
2025-11-13 21:50:37 +0100

    cuvee2025

--- devoidlab/js/footer.js
+++ devoidlab/js/footer.js
@@ -1,8 +1,7 @@
 var element = document.createElement('div');
 var currentYear = new Date().getFullYear();
 element.innerHTML = `
-      <p id="mentions">© Copyright ${currentYear} kodpe.github.io. All rights reserved.
-      kodpe.github.io isn't endorsed by Riot Games Corporation and doesn't reflect the views
+      <p id="mentions">© Copyright ${currentYear} kodpe.github.io. Fan-made, unofficial and non-commercial. kodpe.github.io isn't endorsed by Riot Games Corporation and doesn't reflect the views
       or opinions of Riot Games or anyone officially involved in producing or managing League of Legends.
       League of Legends and Riot Games are trademarks or registered trademarks of Riot Games, Inc.
       League of Legends © Riot Games, Inc.</p>
--- devoidlab/js/main.js
+++ devoidlab/js/main.js
@@ -1,5 +1,5 @@
 document.querySelectorAll('.grid-item').forEach((item, index) => {
     item.addEventListener('click', () => {
-        alert(`Vous avez cliqué sur l'image ${index + 1}`);
+        // alert(`Vous avez cliqué sur l'image ${index + 1}`);
     });
 });
